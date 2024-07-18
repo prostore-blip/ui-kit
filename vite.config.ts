7ab303c9b947d83d59b5903d5ce47903796f6842
Chrizzoman <sasuke123451997@gmail.com>
2024-07-19 00:07:14 +0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { join, resolve } from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+// @ts-ignore
+import { devDependencies, peerDependencies } from './package.json'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, join('src', 'index.ts')),
+      fileName: 'index',
+      formats: ['es', 'cjs'],
+    },
+    minify: false,
+    rollupOptions: {
+      // Exclude peer dependencies from the bundle to reduce bundle size
+      rollupOptions: {
+        external: [
+          ...Object.keys(peerDependencies),
+          ...Object.keys(devDependencies),
+          'react/jsx-runtime',
+        ],
+        output: {
+          dir: 'dist',
+          entryFileNames: '[name].js',
+          format: 'es',
+        },
+      },
+    },
+    target: 'esnext',
+  },
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }), // Output .d.ts files
+  ],
+})

3d82af02d05203002d1be17b085c5bc216ef6894
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-07-20 18:37:10 +0400

    textfield and checkbox

--- src/assets/icons/components/common/check.tsx
+++ src/assets/icons/components/common/check.tsx
@@ -1,19 +1,19 @@
-import { Ref, SVGProps, forwardRef, memo } from 'react'
+import { type Ref, type SVGProps, forwardRef, memo } from 'react'
 
 const SvgComponent = (
   { height = 18, width = 18, ...rest }: SVGProps<SVGSVGElement>,
   ref: Ref<SVGSVGElement>
 ) => (
   <svg
-    fill="currentColor"
+    fill={'currentColor'}
     height={height}
     ref={ref}
-    viewBox="0 -960 960 960"
+    viewBox={'0 -960 960 960'}
     width={width}
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns={'http://www.w3.org/2000/svg'}
     {...rest}
   >
-    <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z" />
+    <path d={'M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z'} />
   </svg>
 )
 const ForwardRef = forwardRef(SvgComponent)

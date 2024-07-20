f1747dc1b2b951b750b5b85aa91d8f2403c00b01
Chrizzoman <sasuke123451997@gmail.com>
2024-07-20 19:39:57 +0300

    refactoring

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,8 @@
 import type { Preview } from '@storybook/react'
 
-import '../src/styles/index.scss'
+import '../src/styles/_colors.scss'
+import '../src/styles/_typography.scss'
+import '../src/styles/_boilerplate.scss'
 
 const preview: Preview = {
   parameters: {

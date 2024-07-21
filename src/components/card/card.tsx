643236581c01d428e6df697640f7bd540546890f
Maximaab <141754764+Maximaab@users.noreply.github.com>
2024-07-21 18:03:00 +0300

    card

---
+++ src/components/card/card.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx'
+
+import type { ComponentPropsWithoutRef, ElementType } from 'react'
+import s from './card.module.scss'
+
+export type CardProps<T extends ElementType = 'div'> = {
+  as?: T
+  className?: string
+  maxWidth?: string
+  variant?: 'dark' | 'light' 
+} & ComponentPropsWithoutRef<T>
+
+export const Card = <T extends ElementType = 'div'>(props: CardProps<T>) => {
+  const {
+    as: Component = 'div',
+    className,
+    maxWidth,
+    variant = 'card',
+    ...rest
+  } = props
+
+  return (
+    <Component
+      className={clsx(s.card, s[variant], className)}
+      style={maxWidth ? { maxWidth } : {}}
+      {...rest}
+    />
+  )
+}

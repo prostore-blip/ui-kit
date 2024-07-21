26738b2d830a176ebc96facf321ffaf0dc87d0ba
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-21 18:22:19 +0300

    IN-18 fix: add stories and refactor captcha for storie Loaded

--- src/components/captcha/captcha.stories.tsx
+++ src/components/captcha/captcha.stories.tsx
@@ -3,15 +3,17 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Captcha } from './captcha'
 
 const meta: Meta<typeof Captcha> = {
-  argTypes: {},
-  component: Captcha,
-  parameters: {
-    docs: {
-      description: {
-        component: 'Captcha',
-      },
-    },
+  args: {
+    error: false,
+    errorTitle: 'string',
+    expired: false,
+    expiredTitle: 'string',
+    isChecked: false,
+    label: "I'm not a robot",
+    load: false,
+    theme: 'light',
   },
+  component: Captcha,
   tags: ['autodocs'],
   title: 'components/Captcha',
 }
@@ -20,14 +22,35 @@ export default meta
 
 type Story = StoryObj<typeof Captcha>
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
-    error: false,
-    errorTitle: 'string',
-    expired: false,
-    expiredTitle: 'string',
-    isChecked: false,
-    label: "I'm not a robot",
-    theme: 'light',
+    theme: 'dark',
+  },
+}
+export const Error: Story = {
+  args: {
+    error: true,
+    errorTitle: 'Please verify that you are not a robot',
+    theme: 'dark',
+  },
+}
+export const Verified: Story = {
+  args: {
+    isChecked: true,
+    theme: 'dark',
+  },
+}
+export const Expireded: Story = {
+  args: {
+    expired: true,
+    expiredTitle: 'Verifiction expired. Check the checkbox again.',
+    theme: 'dark',
+  },
+}
+
+export const Loaded: Story = {
+  args: {
+    load: true,
+    theme: 'dark',
   },
 }

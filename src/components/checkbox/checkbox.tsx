ffe06743b1f749c2f0754f3383ab37f6497a210c
Chrizzoman <sasuke123451997@gmail.com>
2024-07-24 10:42:56 +0300

    input/check

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -1,10 +1,12 @@
 import { type ComponentPropsWithoutRef, type ElementRef, type ReactNode, forwardRef } from 'react'
 
 import * as CheckboxRadix from '@radix-ui/react-checkbox'
-import { CheckboxIndicator } from '@radix-ui/react-checkbox'
+import clsx from 'clsx'
 
-import { Typography } from '../'
-import { Check } from '../../assets/icons/components/check'
+import s from './checkbox.module.scss'
+
+import { Check } from '../../assets/icons/'
+import { Typography } from '../../components/typography/typography'
 
 export type CheckboxProps = {
   label?: ReactNode | string
@@ -12,14 +14,17 @@ export type CheckboxProps = {
 
 export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>(
   ({ className, disabled, label, ...rest }, ref) => {
+    const labelClassName = clsx(s.label, disabled && s.disabled, className)
+
     return (
-      <div>
-        <Typography as={'label'} variant={'regular14'}>
-          <CheckboxRadix.Root ref={ref} {...rest}>
-            <CheckboxIndicator>
-              <Check />
-            </CheckboxIndicator>
+      <div className={s.container}>
+        <Typography as={'label'} className={labelClassName} variant={'regular14'}>
+          <CheckboxRadix.Root className={s.checkbox} disabled={disabled} ref={ref} {...rest}>
+            <CheckboxRadix.Indicator className={s.indicator}>
+              <Check className={disabled ? s.checkIconDisabled : s.checkIcon} />
+            </CheckboxRadix.Indicator>
           </CheckboxRadix.Root>
+          {label}
         </Typography>
       </div>
     )

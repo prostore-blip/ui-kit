0f6e3ff1b8ad736d32aa851b299e515a44e9b0e0
“evan-weissrussland” <evanwodichka@gmail.com>
2024-07-26 22:53:36 +0300

    IN-18 fix: refactor component Captcha for use react-google-recaptcha (v2)

--- src/components/captcha/captcha.tsx
+++ src/components/captcha/captcha.tsx
@@ -1,4 +1,5 @@
-import { type ComponentPropsWithoutRef, useEffect, useState } from 'react'
+import { type ComponentPropsWithoutRef, useEffect, useRef, useState } from 'react'
+import { ReCAPTCHA } from 'react-google-recaptcha'
 
 import clsx from 'clsx'
 
@@ -15,6 +16,7 @@ export type CaptchaProps = {
   label?: string
   load?: boolean
   onClick?: () => void
+  sitekey?: string
   theme?: 'dark' | 'light'
 } & ComponentPropsWithoutRef<'div'>
 
@@ -22,28 +24,54 @@ export const Captcha = ({
   className,
   error = false,
   errorTitle = 'Please verify that you are not a robot',
-  expired = false,
+  expired = false, //только для сторибука
   expiredTitle = 'Verifiction expired. Check the  again.',
-  isChecked,
+  isChecked = false, //только для сторибука
   label = `I'm not a robot`,
-  load = false,
+  load = false, //только для сторибука
   onClick = () => {},
+  sitekey = '',
   theme = 'light',
   ...restProps
 }: CaptchaProps) => {
-  const classNames = clsx(s.captcha, error && s.error, s[theme], className)
-
+  const recaptchaRef = useRef<any>(null)
   const [isLoad, setIsLoad] = useState(false)
+  const [isCheckd, setIsCheckd] = useState(false)
+  const [isExpired, setIsExpired] = useState(false)
+  const [isError, setIsError] = useState(false)
+  /**
+   * @function
+   * Обработчик запуска капчи
+   */
   const loadHandler = () => {
     setIsLoad(true)
-    onClick()
+    if (recaptchaRef.current) {
+      recaptchaRef.current.execute()
+    }
   }
+  /**
+   * для сторибука
+   */
 
   useEffect(() => {
-    if (expired) {
-      setIsLoad(false)
+    setIsLoad(false)
+  }, [expired, isChecked])
+
+  useEffect(() => {
+    if (!isCheckd && !isChecked) {
+      setIsCheckd(false)
     }
-  }, [expired])
+    if (!isCheckd && isChecked) {
+      setIsCheckd(true)
+    }
+    if (isCheckd && !isChecked) {
+      setIsCheckd(false)
+    }
+  }, [isChecked])
+
+  /**
+   * для сторибука
+   */
 
   useEffect(() => {
     if (!isLoad && !load) {
@@ -56,39 +84,66 @@ export const Captcha = ({
       setIsLoad(false)
     }
   }, [load])
+  /**
+   * стили общего контейнера / стили при ошибке / стили для темы
+   */
+  const classNames = clsx(s.captcha, (isError || error) && s.error, s[theme], className)
 
   return (
-    <div className={classNames} {...restProps}>
-      <div className={s.container}>
-        {expired && <span className={clsx(s.expiredTitle)}>{expiredTitle}</span>}
-        <div className={s.blockForInputLabel}>
-          {isChecked && (
-            <div className={s.iconCaptchaChekd}>
-              <CaptchaCheckd />
-            </div>
-          )}
-          {isLoad && (
-            <div className={s.iconCaptchaLoad}>
-              <CaptchaLoad />
-            </div>
-          )}
-          <input
-            className={isChecked || isLoad ? s.checkboxTransparent : ''}
-            id={'captcha-id'}
-            onClick={!isChecked && !isLoad ? loadHandler : undefined}
-            type={'checkbox'}
-          />
-          <label className={clsx()} htmlFor={'captcha-id'}>
-            {label}
-          </label>
+    <>
+      <div className={classNames} {...restProps}>
+        <div className={s.container}>
+          {(isExpired || expired) && <span className={clsx(s.expiredTitle)}>{expiredTitle}</span>}
+          <div className={s.blockForInputLabel}>
+            {(isCheckd || isChecked) && (
+              <div className={s.iconCaptchaChekd}>
+                <CaptchaCheckd />
+              </div>
+            )}
+            {isLoad && (
+              <div className={s.iconCaptchaLoad}>
+                <CaptchaLoad />
+              </div>
+            )}
+            <input
+              className={isCheckd || isLoad ? s.checkboxTransparent : ''}
+              id={'captcha-id'}
+              onClick={!isCheckd && !isLoad ? loadHandler : undefined}
+              type={'checkbox'}
+            />
+            <label className={clsx()} htmlFor={'captcha-id'}>
+              {label}
+            </label>
+          </div>
+          <div className={s.figure}>
+            <ReCaptcha />
+            <span className={clsx(s.descriptionReCAPTCHA)}>reCAPTCHA</span>
+            <span className={clsx(s.descriptionPrivacyTerms)}>Privacy - Terms</span>
+          </div>
         </div>
-        <div className={s.figure}>
-          <ReCaptcha />
-          <span className={clsx(s.descriptionReCAPTCHA)}>reCAPTCHA</span>
-          <span className={clsx(s.descriptionPrivacyTerms)}>Privacy - Terms</span>
-        </div>
+        {(isError || error) && <span className={clsx(s.errorTitle)}>{errorTitle}</span>}
       </div>
-      {error && <span className={clsx(s.errorTitle)}>{errorTitle}</span>}
-    </div>
+      <ReCAPTCHA
+        onChange={() => {
+          //капча успешно пройдена
+          setIsLoad(false)
+          setIsCheckd(true)
+          onClick()
+        }}
+        onErrored={() => {
+          //только ошибка сети. Если неправильно вводить карчу, то не срабатывает
+          setIsLoad(false)
+          setIsError(true)
+        }}
+        onExpired={() => {
+          //капча протухла. Через 2 минуты после успешного срабатывания
+          setIsExpired(true)
+          setIsCheckd(false)
+        }}
+        ref={recaptchaRef}
+        sitekey={sitekey}
+        size={'invisible'}
+      />
+    </>
   )
 }

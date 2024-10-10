c24272d5fb7332c1cbcdf199cb7a984c7bc33f0e
Aleksandr Savkin <voyager5874@gmail.com>
2024-10-10 11:14:37 +0300

    refactor: styles for toasts types

--- src/components/toaster/toaster.stories.tsx
+++ src/components/toaster/toaster.stories.tsx
@@ -2,15 +2,14 @@ import type { Meta, StoryObj } from '@storybook/react'
 
 import { Toaster, toast } from 'sonner'
 
-import '@fontsource/inter'
-
 import s from './toaster.module.scss'
 
 import { Toast } from './toaster'
 
 const meta: Meta<typeof Toast> = {
   args: {
-    title: 'aaaaaaa ddddddddd dddd',
+    title:
+      'Lorem ipsum dolor sit amet et nulla aliquyam velit erat gubergren invidunt et et lorem et eum dolor.',
   },
   component: Toast,
   tags: ['autodocs'],
@@ -20,21 +19,21 @@ const meta: Meta<typeof Toast> = {
 export default meta
 
 type Story = StoryObj<typeof Toast>
-const onClick = (title: string, isSuccess: boolean) => {
-  toast.custom(jsx => <Toast onDismiss={() => toast.dismiss(jsx)} title={title} />, {
-    className: isSuccess ? s.succesToast : s.errorToast,
+const onClick = (title: string, eventType: 'error' | 'info' | 'success' | 'warning') => {
+  toast.custom(toastId => <Toast onDismiss={() => toast.dismiss(toastId)} title={title} />, {
+    className: s[eventType],
     duration: Infinity,
   })
 }
 
-export const SuccessToastStory: Story = {
+export const SuccessToast: Story = {
   render: ({ title }) => {
     return (
       <>
         <Toaster position={'bottom-left'} />
         <button
           onClick={() => {
-            onClick(title, true)
+            onClick(title, 'success')
           }}
           type={'button'}
         >
@@ -44,14 +43,50 @@ export const SuccessToastStory: Story = {
     )
   },
 }
-export const ErrorToastStory: Story = {
+export const ErrorToast: Story = {
   render: ({ title }) => {
     return (
       <>
         <Toaster position={'bottom-left'} />
         <button
           onClick={() => {
-            onClick(title, false)
+            onClick(title, 'error')
+          }}
+          type={'button'}
+        >
+          click me
+        </button>
+      </>
+    )
+  },
+}
+
+export const WarningToast: Story = {
+  render: ({ title }) => {
+    return (
+      <>
+        <Toaster position={'bottom-left'} />
+        <button
+          onClick={() => {
+            onClick(title, 'warning')
+          }}
+          type={'button'}
+        >
+          click me
+        </button>
+      </>
+    )
+  },
+}
+
+export const infoToast: Story = {
+  render: ({ title }) => {
+    return (
+      <>
+        <Toaster position={'bottom-left'} />
+        <button
+          onClick={() => {
+            onClick(title, 'info')
           }}
           type={'button'}
         >

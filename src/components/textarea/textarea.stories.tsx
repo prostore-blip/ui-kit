27367ed2934aadb44ca72e7d18672286e0145db2
Aleksandr Savkin <voyager5874@gmail.com>
2024-10-14 01:08:39 +0300

    refactor: text-area

--- src/components/textarea/textarea.stories.tsx
+++ src/components/textarea/textarea.stories.tsx
@@ -1,37 +1,59 @@
-import { Meta, StoryObj } from '@storybook/react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import { TextArea } from './textarea'
 
 const meta = {
+  argTypes: {
+    autoHeight: {
+      control: 'boolean',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+    errorMessage: {
+      control: 'text',
+    },
+    label: {
+      control: 'text',
+    },
+    placeholder: {
+      control: 'text',
+    },
+    required: {
+      control: 'boolean',
+    },
+    resizeable: {
+      control: 'radio',
+      options: ['both', 'vertical', 'horizontal', 'none'],
+    },
+    rows: {
+      control: 'number',
+    },
+  },
   component: TextArea,
   tags: ['autodocs'],
-  title: 'Components/Textarea',
+  title: 'Components/TextArea',
 } satisfies Meta<typeof TextArea>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const TextAreaDefault: Story = {
+export const Overview: Story = {
   args: {
-    height: '100px',
-    placeholder: 'Texr Area',
-    width: '300px',
-  },
-}
-export const TextAreaDisablet: Story = {
-  args: {
-    disabled: true,
-    height: '100px',
-    placeholder: 'Texr Area',
-    width: '250px',
+    autoHeight: true,
+    placeholder: 'placeholder text',
   },
 }
 
-export const TextAreaError: Story = {
+export const WithText: Story = {
   args: {
-    error: true,
-    height: '100px',
-    placeholder: 'Texr Area',
-    width: '250px',
+    autoHeight: true,
+    value: `
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
+    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
   },
 }
--- src/components/textarea/textarea.tsx
+++ src/components/textarea/textarea.tsx
@@ -1,31 +1,222 @@
-import type { ComponentPropsWithoutRef, ElementType } from 'react'
+import {
+  ChangeEvent,
+  ComponentPropsWithoutRef,
+  ElementRef,
+  FocusEvent,
+  KeyboardEvent,
+  MouseEvent,
+  RefObject,
+  forwardRef,
+  useEffect,
+  useId,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react'
 
-import clsx from 'clsx'
+import { clsx } from 'clsx'
 
 import s from './textarea.module.scss'
 
-export type TextAreatype<T extends ElementType = 'textarea'> = {
-  className?: string
-  error?: true
-  height?: string
-  placeholder: string
-  width?: string
-} & ComponentPropsWithoutRef<T>
+import { Typography } from '../typography'
 
-export const TextArea = <T extends ElementType = 'textarea'>(props: TextAreatype<T>) => {
-  const { as: Component = 'textarea', className, error, height, width, ...rest } = props
+export type CustomProps = {
+  autoHeight?: boolean
+  errorMessage?: string
+  label?: string
+  maxHeight?: number
+  onEnter?: () => void
+  onValueChange?: (value: string) => void
+  resizeable?: 'both' | 'horizontal' | 'none' | 'vertical'
+}
 
-  return (
-    <Component
-      className={clsx(
-        s.textarea,
-        {
-          [s.error]: error,
-        },
-        className
-      )}
-      style={{ height, width }}
-      {...rest}
-    />
-  )
+export type TextAreaProps = CustomProps &
+  Omit<ComponentPropsWithoutRef<'textarea'>, keyof CustomProps>
+
+export const TextArea = forwardRef<ElementRef<'textarea'>, TextAreaProps>(
+  (
+    {
+      autoHeight = false,
+      className,
+      disabled,
+      errorMessage,
+      id,
+      label,
+      maxHeight,
+      onBlur,
+      onChange,
+      onClick,
+      onEnter,
+      onValueChange,
+      required,
+      resizeable = 'none',
+      rows,
+      value,
+      ...restProps
+    },
+    forwardedRef
+  ) => {
+    const [active, setActive] = useState(false)
+
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    const initialHeight = useRef<null | number>(null)
+
+    useEffect(() => {
+      if (textAreaRef?.current) {
+        if (!textAreaRef?.current?.value) {
+          initialHeight.current = textAreaRef.current.offsetHeight
+        }
+        if (textAreaRef?.current?.value) {
+          const requiredHeight = getRequiredHeight(textAreaRef)
+
+          if (maxHeight && autoHeight && requiredHeight) {
+            requiredHeight <= maxHeight
+              ? (textAreaRef.current.style.height = `${requiredHeight}px`)
+              : (textAreaRef.current.style.height = `${maxHeight}px`)
+          }
+          if (!maxHeight && autoHeight) {
+            requiredHeight && (textAreaRef.current.style.height = `${requiredHeight + 12}px`)
+          }
+        }
+      }
+    }, [])
+
+    useImperativeHandle(
+      forwardedRef,
+      () =>
+        ({
+          ...textAreaRef.current,
+        }) as HTMLTextAreaElement
+    )
+
+    const autoId = useId()
+
+    if (maxHeight && !autoHeight) {
+      throw new Error('maxHeight makes sense only with autoHeight=true')
+    }
+    if (maxHeight && maxHeight < 0) {
+      throw new Error('Wrong maxHeight value')
+    }
+
+    if (maxHeight && rows && initialHeight?.current && initialHeight.current > maxHeight) {
+      throw new Error(
+        `Wrong maxHeight value: pass maxHeight value which exceeds ${initialHeight.current}px`
+      )
+    }
+
+    const handleAreaTextChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
+      !active && setActive(true)
+      onChange && onChange(event)
+      onValueChange && onValueChange(event.currentTarget.value)
+      if (!autoHeight) {
+        return
+      }
+      if (!textAreaRef?.current) {
+        return
+      }
+
+      const currentHeight = textAreaRef.current.offsetHeight
+
+      if (!maxHeight) {
+        const requiredHeight = getRequiredHeight(textAreaRef)
+
+        textAreaRef.current.style.height =
+          currentHeight > requiredHeight ? `${currentHeight}px` : `${requiredHeight}px`
+
+        return
+      }
+
+      if (currentHeight && maxHeight) {
+        const requiredHeight = getRequiredHeight(textAreaRef)
+
+        if (requiredHeight <= maxHeight && currentHeight < requiredHeight) {
+          textAreaRef.current.style.height = `${requiredHeight}px`
+        } else {
+          textAreaRef.current.style.height = `${currentHeight}px`
+        }
+      }
+    }
+
+    const handleEnterKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
+      if (onEnter && event.key === 'Enter') {
+        onEnter()
+      }
+    }
+
+    const handleClick = (e: MouseEvent<HTMLTextAreaElement>) => {
+      onClick && onClick(e)
+      setActive(true)
+    }
+
+    const handleBlur = (e: FocusEvent<HTMLTextAreaElement>) => {
+      onBlur && onBlur(e)
+      setActive(false)
+    }
+
+    const cn = {
+      area: clsx(s.area, active && s.active, errorMessage && s.errorInput, className),
+      container: clsx(s.container, disabled && s.disabled),
+      error: clsx(s.errorText, !errorMessage && s.transparent),
+      label: clsx(s.label, disabled && s.disabled),
+    }
+
+    return (
+      <div className={cn.container}>
+        <Typography as={'label'} className={cn.label} htmlFor={id || autoId} variant={'regular14'}>
+          <span>{label}</span>
+          {required && <span className={s.errorText}>*</span>}
+        </Typography>
+        <textarea
+          aria-describedby={`${id || autoId}-field-message`}
+          className={cn.area}
+          disabled={disabled}
+          id={id || autoId}
+          onBlur={handleBlur}
+          onChange={handleAreaTextChange}
+          onClick={handleClick}
+          onKeyDown={handleEnterKeyPress}
+          ref={textAreaRef}
+          required={required}
+          rows={rows || 1}
+          style={{ resize: resizeable }}
+          value={value}
+          {...restProps}
+        />
+        {errorMessage && (
+          <Typography
+            aria-live={'assertive'}
+            className={cn.error}
+            id={`${id || autoId}-field-message`}
+            variant={'small'}
+          >
+            {errorMessage}
+          </Typography>
+        )}
+        {!errorMessage && (
+          <Typography className={s.transparent} variant={'regular14'}>
+            placeholder
+          </Typography>
+        )}
+      </div>
+    )
+  }
+)
+
+function getRequiredHeight(ref: RefObject<any>) {
+  if (!ref?.current) {
+    return null
+  }
+  const styles = getComputedStyle(ref?.current)
+  const valueString = styles.getPropertyValue('padding')
+  const paddingNumber = Number.parseInt(valueString)
+
+  const offsetHeight = ref.current.offsetHeight
+
+  ref.current.style.height = '0px'
+  const { scrollHeight: requiredHeight } = ref.current
+
+  ref.current.style.height = `${offsetHeight}px`
+
+  return requiredHeight + paddingNumber
 }
